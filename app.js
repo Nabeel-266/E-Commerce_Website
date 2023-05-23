90f74ffc1c01779caa74400aafeed545e9ee2f97
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2023-05-23 21:59:09 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const navItems = document.querySelectorAll('.navLink');
+// // console .log(navItems);
+// const navLinkItem = Array.from(navItems);
+// console.log(navLinkItem);
+
+// navLinkItem.forEach( (item) => {
+//     item.addEventListener('click', function() {
+//         navLinkItem.forEach(nav => nav.classList.remove('activeLink'));
+
+//         this.classList.add('activeLink');
+//     });
+//     // console.log(item)
+// })
